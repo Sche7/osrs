dab832bc67b615d842471910bdc8071f6349d0f8
Steven <stevencheung7@outlook.dk>
2023-12-21 21:36:13 +0100

    minor

--- tests/core/api/test_hiscores.py
+++ tests/core/api/test_hiscores.py
@@ -65,6 +65,12 @@ def test_calculate_combat_level(
 def test_get_character_stats(username: str):
     """Test the get_character_stats method."""
     hiscores = Hiscores(username)
+
+    # Since the character stats are always changing, we can't really test
+    # for a specific value. We can however test that the values are greater
+    # than a certain value. As these characters exists, we can assume that
+    # they will always have a combat level greater than 3 and a total level
+    # greater than 32.
     assert hiscores.character.combat_level > 3
     assert hiscores.character.total_level > 32
     assert hiscores.character.username == username
