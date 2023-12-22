5f9abe41efdb7c3976cbae31801ce8c11b1c9f36
Steven <stevencheung7@outlook.dk>
2023-12-22 08:51:10 +0100

    unflatten

--- core/utils/dict.py
+++ core/utils/dict.py
@@ -10,3 +10,16 @@ def flatten(dictionary, parent_key="", separator="."):
         else:
             items.append((new_key, value))
     return dict(items)
+
+
+def unflatten(dictionary, separator="."):
+    items = {}
+    for key, value in dictionary.items():
+        parts = key.split(separator)
+        d = items
+        for part in parts[:-1]:
+            if part not in d:
+                d[part] = {}
+            d = d[part]
+        d[parts[-1]] = value
+    return items
--- tests/core/utils/test_dict.py
+++ tests/core/utils/test_dict.py
@@ -1,6 +1,11 @@
-from core.utils.dict import flatten
+from core.utils.dict import flatten, unflatten
 
 
 def test_flatten():
     d = {"a": 1, "b": {"c": 2, "d": {"e": 3, "f": 4}, "g": 5}}
     assert flatten(d) == {"a": 1, "b.c": 2, "b.d.e": 3, "b.d.f": 4, "b.g": 5}
+
+
+def test_unflatten():
+    d = {"a": 1, "b.c": 2, "b.d.e": 3, "b.d.f": 4, "b.g": 5}
+    assert unflatten(d) == {"a": 1, "b": {"c": 2, "d": {"e": 3, "f": 4}, "g": 5}}
