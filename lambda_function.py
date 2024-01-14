dedcae3386bcec2ee32fff1615d2d040cf3d5c3f
Sche7 <stevencheung7@outlook.dk>
2024-01-14 15:31:48 +0100

    format

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,10 @@
 import json
 from src.webhooks.discord import main, USERNAMES, BUCKET_NAME, REMOTE_FOLDER
 
+
 def lambda_handler(event, context):
     main(USERNAMES, BUCKET_NAME, REMOTE_FOLDER)
     return {
-        'statusCode': 200,
-        'body': json.dumps(f"Successfully sent webhook to Discord.")
+        "statusCode": 200,
+        "body": json.dumps(f"Successfully sent webhook to Discord."),
     }
