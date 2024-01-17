ee40e1d2ad7ca2de40d4863312964c24491ddda7
Sche7 <stevencheung7@outlook.dk>
2024-01-17 19:49:45 +0100

    rename folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "discord_hook" {
-    source = "./discord_hook"
+    source = "./discord_webhook"
     osrs_usernames = var.osrs_usernames
     osrs_remote_folder = var.osrs_remote_folder
     discord_webhook_url = var.discord_webhook_url
