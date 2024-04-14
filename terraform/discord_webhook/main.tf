95876f9700e6b727ddb6b1e4b53b41910fc0406c
Sche7 <stevencheung7@outlook.dk>
2024-04-14 14:37:08 +0200

    update cron expression

--- terraform/discord_webhook/main.tf
+++ terraform/discord_webhook/main.tf
@@ -136,7 +136,7 @@ resource "aws_lambda_function" "osrs_lambda" {
 resource "aws_cloudwatch_event_rule" "osrs_lambda_event" {
     name                = "osrs_lambda_event"
     description         = "Event for osrs lambda"
-    schedule_expression = "rate(7 days)"
+    schedule_expression = "cron(0 8 ? * MON *)"
 }
 
 # Create a target lambda function for the rule
