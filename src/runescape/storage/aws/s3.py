6b2918a1f6d832036fddac18650b4964084117ea
Sche7 <stevencheung7@outlook.dk>
2025-02-09 11:11:33 +0100

    minor

--- src/runescape/storage/aws/s3.py
+++ src/runescape/storage/aws/s3.py
@@ -2,13 +2,16 @@ from runescape.api.aws.s3 import S3
 from runescape.storage.protocol import StorageProtocol
 
 
+DEFAULT_REGION_NAME = "eu-north-1"
+
+
 class S3Storage(StorageProtocol):
     def __init__(
         self,
         aws_access_key_id: str,
         aws_secret_access_key: str,
         bucket_name: str,
-        region_name: str = "eu-north-1",
+        region_name: str = DEFAULT_REGION_NAME,
     ):
         self.s3 = S3(aws_access_key_id, aws_secret_access_key, region_name)
         self.bucket_name = bucket_name
