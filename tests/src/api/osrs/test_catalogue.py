ea48c0fc12ed07075a47a1ff181e806ec9be699f
Sche7 <stevencheung7@outlook.dk>
2025-11-11 22:33:13 +0100

    add tests

--- tests/src/api/osrs/test_catalogue.py
+++ tests/src/api/osrs/test_catalogue.py
@@ -1,7 +1,9 @@
 import pytest
+from httpx import HTTPStatusError
 
 from runescape.api.osrs.catalogue import OSRSCatalogue
 from runescape.api.osrs.models import Categories
+from runescape.dataclasses.categories import Tradeables
 from runescape.dataclasses.items import Items
 
 
@@ -9,6 +11,7 @@ from runescape.dataclasses.items import Items
     ("category", "alpha", "page"),
     [
         (1, "a", 3),
+        ("1", "a", "3"),
         (3, "z", 1),
         (Categories.AMMO, "a", 1),
     ],
@@ -17,3 +20,31 @@ def test_catalogue_items(category, alpha, page):
     client = OSRSCatalogue()
     result = client.items(category=category, alpha=alpha, page=page)
     assert isinstance(result, Items)
+
+
+def test_catalogue_items_exception():
+    client = OSRSCatalogue()
+
+    with pytest.raises(HTTPStatusError):
+        client.items(
+            category=1000,  # Non-existence category
+            alpha="a",
+            page=1,
+        )
+
+
+@pytest.mark.parametrize(
+    "category",
+    [
+        Categories.AMMO,
+        Categories.ARCHAEOLOGY_MATERIALS,
+        Categories.ARROWS,
+        0,
+        1,
+        2,
+    ],
+)
+def test_catalogue_categories(category: Categories):
+    client = OSRSCatalogue()
+    result = client.categories(category=category)
+    assert isinstance(result, Tradeables)
